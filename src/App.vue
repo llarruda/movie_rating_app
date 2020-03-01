1775eea5e0cc15e2e804786efb785f34ad185b4c
llarruda <leandroarruda1977@gmail.com>
2020-03-01 15:18:33 -0300

    Initial commit - a fresh vue/cli project, develop branch.

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app-bar id="inspire">
+    <v-navigation-drawer
+     fixed
+     v-model="drawer"
+     app
+    >
+      <v-list dense>
+        <router-link
+         v-bind:to="{ name: 'Home' }"
+         class="side_bar_link"
+        >
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>home</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>Home</v-list-item-content>
+          </v-list-item>
+        </router-link>
+        <router-link v-bind:to="{ name: 'Contact'}" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>Contact</v-list-item-content>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drower = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Home</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-conteiner fluid>
+        <div id="app">
+          <router-view/>
+        </div>
+      </v-conteiner>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white-text">&copy; 2020</span>
+    </v-footer>
+  </v-app-bar>
+</template>
+
+<script>
+import './assets/stylesheets/main.css';
+
+export default {
+  data: () => ({
+    drawer: null,
+  }),
+  props: {
+    source: String,
+  },
+};
+</script>
+
+<style>
+</style>
