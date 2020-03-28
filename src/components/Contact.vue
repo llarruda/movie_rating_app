abba9efbdc4c4f8891413cc002504fd96ce82193
llarruda <leandroarruda1977@gmail.com>
2020-03-28 20:33:55 -0300

    Make changes at Contact component to see how to chance the grid shape.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,52 @@
 <template>
-  <v-layout>
-    this is contact
+  <v-layout row-wrap>
+     <v-flex xs4>
+      <v-card>
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Lorem ipsum title</div>
+            <span class="grey--text">2016. Science fiction film/action 3h 3m</span>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          It's been nearly two years since Superman's (Henry Cavill)
+colossal battle with Zod (Michael Shannon) devastated the city of
+Metropolis. The loss of life and collateral damage left many feeling angry
+and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+Affleck). Convinced that Superman is now a threat to humanity, Batman
+embarks on a personal vendetta to end his reign on Earth, while the
+conniving Lex
+        </v-card-text>
+        <v-card-action>
+          <b-btn flat color="purple">Rate this movie</b-btn>
+          <v-space></v-space>
+        </v-card-action>
+      </v-card>
+    </v-flex>
+    <div>
+    </div>
+    <v-flex xs4>
+      <v-card>
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Lorem ipsum title</div>
+            <span class="grey--text">2016. Science fiction film/action 3h 3m</span>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          It's been nearly two years since Superman's (Henry Cavill)
+colossal battle with Zod (Michael Shannon) devastated the city of
+Metropolis. The loss of life and collateral damage left many feeling angry
+and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+Affleck). Convinced that Superman is now a threat to humanity, Batman
+embarks on a personal vendetta to end his reign on Earth, while the
+conniving Lex
+        </v-card-text>
+        <v-card-action>
+          <b-btn flat color="purple">Rate this movie</b-btn>
+          <v-space></v-space>
+        </v-card-action>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
